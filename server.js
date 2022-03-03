01c8ed72457348a433cf34e756a528fd92867ef3
unknown <uddeshteke007@gmail.com>
2022-03-04 01:57:32 +0530

    server file added

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const { request } = require('express');
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req,res) => res.send('API Running'));
+// Connect DB
+connectDB();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server started on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
