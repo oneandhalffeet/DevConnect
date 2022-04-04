69e884fc583744aadffdb1d4f9a0d5a315021127
unknown <uddeshteke007@gmail.com>
2022-04-04 21:00:48 +0530

    Added route for Deletion of profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -146,5 +146,23 @@ router.get('/user/:user_id', async(req,res)=>{
     }
 });
 
+//@route    DELETE api/profile
+//@desc     Delete profile, user and post
+//@access   Private 
+
+router.delete('/',auth, async(req,res)=>{
+    try {
+        // @todo - remove user post
+        //remove profile from data
+        await Profile.findOneAndRemove({ user: req.user.id});
+        //remove user
+        await User.findOneAndRemove({ _id: req.user.id});
+        res.json({ msg: 'User deleted'});
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;
