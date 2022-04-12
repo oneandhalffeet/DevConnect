46e8fd7c12a4210a1594a1d792940c0864ba41e4
unknown <uddeshteke007@gmail.com>
2022-04-12 22:15:16 +0530

    Added landing page and css styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React, { Fragment } from 'react';
+import { Navbar } from './components/layout/Navbar';
+import { Landing } from './components/layout/Landing';
 
 const App = () => {
   return (
     <Fragment>
-      <h1>App</h1>
+      <Navbar />
+      <Landing />
     </Fragment>
   );
 };
