8a784d9a562c3a3c58550beaf5b0194a13b811cd
unknown <uddeshteke007@gmail.com>
2022-05-23 01:58:23 +0530

    Added frontend changes and packahe modification

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,61 @@
 import React from 'react';
+import { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData + '\n:Success');
+  };
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Log In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Log In Account
+      </p>
+      <form
+        className='form'
+        action='create-profile.html'
+        onSubmit={(e) => onSubmit(e)}
+      >
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
+            minLength='6'
+            required
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Dont have an account? <Link to='/Register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
+
+export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
   const [formData, setFormData] = useState({
@@ -14,12 +15,12 @@ export const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    if (password != password2) {
+    if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      console.log(formData);
+      console.log(formData + '\n:Success');
     }
   };
   return (
@@ -81,8 +82,10 @@ export const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/Login'>Log In</Link>
       </p>
     </Fragment>
   );
 };
+
+export default Register;
